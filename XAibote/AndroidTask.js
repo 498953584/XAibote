a8a438f02a9ee00f91b0247d8dbaacf11219782b
LeoXiong <498953584@qq.com>
2023-01-30 22:02:57 +0800

    nodejs封装模块提交

---
+++ XAibote/AndroidTask.js
@@ -0,0 +1,35 @@
+const AndroidBot = require('AndroidBot');//引用AndroidBot模块
+
+//注册主函数，安卓端连接脚本会自动调用androidMain，并传递AndroidBot对象。设置服务端监听端口，手机端默认连接端口16678
+AndroidBot.registerMain(androidMain, 16678);
+
+/**用作代码提示，androidMain函数会被多次调用，注意使用全局变量
+* @param {AndroidBot} androidBot
+*/
+async function androidMain(androidBot) {
+    await androidBot.setImplicitTimeout(3000);//设置隐式等待时间
+    let androidId = await androidBot.getAndroidId();
+    console.log(androidId);
+    // setInterval(async function () {
+    //     let speed = await androidBot.getElementText("com.android.systemui/com.android.systemui:id=network_speed_contentview");
+    //     console.log(speed);
+    // }, 1000);
+    // console.time('今日');
+    // let words = await androidBot.findWords("今日");
+    await androidBot.startApp("com.tencent.mobileqq");
+    await androidBot.clickElement("com.tencent.mobileqq/com.tencent.mobileqq:id=conversation_head");
+    if (await androidBot.existsElement("com.tencent.mobileqq/android.widget.TextView@containsText=已打卡")) {
+        console.log("");
+        await androidBot.manyBack(2);
+        return;
+    }
+    await androidBot.clickElement("com.tencent.mobileqq/android.widget.TextView@containsText=打卡");
+    await androidBot.clickElement("com.tencent.mobileqq/android.widget.Button@containsText=立即打卡");
+    await androidBot.manyBack(5);
+    // let txt = await androidBot.getElementText("com.samsung.android.app.sreminder/com.samsung.android.app.sreminder:id=tv_text");
+    // console.log(txt);
+    // await androidBot.setElementText("com.samsung.android.app.sreminder[1]/com.samsung.android.app.sreminder:id=search_view", "ssss\n");
+    // await androidBot.sendVk(66);
+    // console.timeEnd('今日');
+    // console.log(words);
+}
--- testWebBot.js
+++ testWebBot.js
@@ -2,7 +2,7 @@ const WebBot = require('WebBot');//引用WebBot模块
 
 //注册主函数，指定浏览器相关参数
 WebBot.registerMain(webMain, "127.0.0.1", 36678, {
-    browserName: "edge",
+    browserName: "edge", 
     // debugPort: 19222
 });
 
@@ -13,7 +13,7 @@ async function webMain(webBot) {
     //设置隐式等待
     await webBot.setImplicitTimeout(5000);
     while (true) {
-        console.log("打开https://rewards.bing.com/pointsbreakdown，开始检查今日搜索任务完成情况...");
+        console.log("开始检查今日搜索任务完成情况...");
         await webBot.goto("https://rewards.bing.com/pointsbreakdown");
         let txtBreak = await webBot.getElementInnerHTML('//*[@id="userPointsBreakdown"]/div/div[2]/div/div[1]/div/div[2]/mee-rewards-user-points-details/div/div/div/div/p[3]');
         let txtNum = await webBot.getElementInnerHTML('//*[@id="userPointsBreakdown"]/div/div[2]/div/div[1]/div/div[2]/mee-rewards-user-points-details/div/div/div/div/p[2]/b');
@@ -22,20 +22,20 @@ async function webMain(webBot) {
             return;
         }
         let txt = txtBreak.replace(new RegExp(/.*最多可获得 (\d+) 分.*搜索可得 (\d+) 分/, "g"), "$1|$2");
-        console.log("今日已获得:" + txtNum, txt);
+        console.log(txt, txtNum);
         let nums = txt.split("|");
         let num = (parseInt(nums[0]) - parseInt(txtNum)) / parseInt(nums[1]);
-        console.log("搜索剩余次数：" + num);
+        console.log(num);
         if (num == 0) {
             console.log("已全部完成！");
             break;
         }
-        for (var i = 0; i < num; i++) {
-            await webBot.goto("https://cn.bing.com/search?q=天气" + i + Math.random());
+        for (var i = 0; i < 5; i++) {
+            await webBot.goto("https://cn.bing.com/search?q=天气" + i);
             // await webBot.sendKeys('//*[@id="sb_form_q"]', '今天的天气' + i);
             // await webBot.sendVk(13);
-            await webBot.isDisplayed('//*[@id="rh_meter"]');
-            await webBot.sleep(1000);
+            await webBot.clickElement('//*[@id="rh_meter"]');
+            await webBot.setImplicitTimeout(2000);
         }
     }
     // //注入js代码并返回值
